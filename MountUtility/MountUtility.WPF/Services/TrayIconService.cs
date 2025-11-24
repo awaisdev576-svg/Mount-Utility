86b0452460e4c2cd4c1fc06ee00ee6cab4d2f3fa
awaisdev576-svg <awaisdev576@gmail.com>
2025-11-24 19:38:24 +0500

    Add files via upload

---
+++ MountUtility/MountUtility.WPF/Services/TrayIconService.cs
@@ -0,0 +1,73 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using MountUtility.WPF.Views;
+using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MountUtility.WPF.Service
+{
+    public class TrayIconService : IDisposable
+    {
+        private readonly MainWindow _mainWindow;
+        private TaskbarIcon? _trayIcon;
+        private bool _disposed;
+
+        public TrayIconService(MainWindow mainWindow)
+        {
+            _mainWindow = mainWindow;
+        }
+
+        public void Initialize()
+        {
+            _trayIcon = new TaskbarIcon
+            {
+                ToolTipText = "Disk Mount Utility"
+            };
+
+            string iconPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "app.ico");
+            if (!System.IO.File.Exists(iconPath))
+                throw new InvalidOperationException($"Tray icon not found at {iconPath}");
+
+            _trayIcon.Icon = new Icon(iconPath);
+
+            var menu = new ContextMenu();
+
+            var openItem = new MenuItem { Header = "Open" };
+            openItem.Click += (s, e) => ShowMainWindow();
+
+            var exitItem = new MenuItem { Header = "Exit" };
+            exitItem.Click += (s, e) =>
+            {
+                Dispose();
+                Application.Current.Shutdown();
+            };
+
+            menu.Items.Add(openItem);
+            menu.Items.Add(new Separator());
+            menu.Items.Add(exitItem);
+
+            _trayIcon.ContextMenu = menu;
+
+            _trayIcon.TrayMouseDoubleClick += (s, e) => ShowMainWindow();
+        }
+
+        private void ShowMainWindow()
+        {
+            if (!_mainWindow.IsVisible)
+                _mainWindow.Show();
+
+            if (_mainWindow.WindowState == WindowState.Minimized)
+                _mainWindow.WindowState = WindowState.Normal;
+
+            _mainWindow.Activate();
+        }
+
+        public void Dispose()
+        {
+            if (_disposed) return;
+            _trayIcon?.Dispose();
+            _disposed = true;
+        }
+    }
+}
